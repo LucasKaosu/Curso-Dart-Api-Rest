e5a3225d3ae9a1485afb2f000bf4a0df1ab74048
LucasKaosu <lukbrowww@hotmail.com>
2023-04-19 20:39:44 -0300

    Módulo 15-Mixin - Concluído

---
+++ 15-Mixins/MixinOn.dart
@@ -0,0 +1,73 @@
+/*
+  * Conceito
+  - Mixin permite implementar métodos e atributos de outras classes
+  * Regras
+  - Devem ser implementados antes das interfaces
+  - Podem ser implementados a partir de uma classe concreta ou abstrata
+  - Não pode implementar uma classe que estenda e nem extender de uma classe mixin 
+  - Pode implementar mais de um mixin, e não é obrigatorio usar todos métodos e atributos
+  - As classes que possuem métodos iguais serão sobrescritas em ordem até a classe herdeira
+  * ON
+  - É obrigatório declarar sua classe como mixin.
+  - Restringe o uso do mixin as classes que estendem ou implementam a classe declarada.
+*/
+
+abstract class Artista {
+  void acao() {
+    print('Performista...');
+  }
+}
+
+// Mixins
+
+mixin Cantor on Artista {
+  void acao() {
+    print('Canta');
+  }
+}
+
+class Dancarino {
+  void acao() {
+    print('Dança');
+  }
+}
+
+// Interface
+
+abstract class Acao {
+  void executar();
+}
+
+// Classe Concreta
+
+class Musico extends Artista with Dancarino, Cantor implements Acao {
+  void acao() => print('Compõe\n'); // Comente para ver a hierarquia de sobrescrita!
+
+  @override
+  void executar() {
+    super.acao();
+    acao();
+  }
+}
+
+// A classe MC não pode cantar porque não extends de Artista
+
+class MC with Dancarino implements Acao {
+  void acao() => print('Mixa, solta o batidão\n'); // Comente para ver a hierarquia de sobrescrita!
+
+  @override
+  void executar() {
+    super.acao();
+    acao();
+  }
+}
+
+void main() {
+  print('15.1) Mixin ON\n');
+
+  Musico musico = Musico();
+  musico.executar(); // Compõe
+
+  MC mc = MC();
+  mc.executar(); // Bota o batidão
+}
---
+++ 15-Mixins/Mixins.dart
@@ -0,0 +1,115 @@
+/*
+  * Conceito
+  - Mixin permite implementar métodos e atributos de outras classes
+  * Regras
+  - Devem ser implementados antes das interfaces
+  - Podem ser implementados a partir de uma classe concreta ou abstrata
+  - Não pode implementar uma classe que estenda e nem extender de uma classe mixin 
+  - Pode implementar mais de um mixin, e não é obrigatorio usar todos métodos e atributos
+  - As classes que possuem métodos iguais serão sobrescritas em ordem até a classe herdeira
+  * ON
+  - É obrigatório declarar sua classe como mixin.
+  - Restringe o uso do mixin as classes que estendem ou implementam a classe declarada.
+*/
+
+abstract class Cidadao {
+  String? nome;
+
+  Cidadao(this.nome);
+
+  void objetivosPessoais();
+  void direitosDeveres() {
+    print('Todo cidadão tem direitos e deveres');
+  }
+}
+
+// INTERFACES
+
+abstract class Presidenciavel {
+  String? partido;
+  String? ideologia;
+
+  void idelologiaPolitica();
+}
+
+class Postagem {
+  String? postagem;
+
+  void escrevePostagem() {
+    print('');
+  }
+}
+
+// MIXINS
+
+mixin Elegivel on Cidadao {
+  bool elegivel = false;
+  void prestacaoContas();
+}
+
+abstract class Conta {
+  double _saldo = 0;
+  double salario = 33000;
+
+  get saldo => this._saldo;
+  set depositar(double valor) => this._saldo = valor;
+
+  bool declaracaoRenda() => (_saldo / 12) < salario;
+}
+
+// CLASSE CONCRETA
+class Candidato extends Cidadao with Elegivel, Conta implements Postagem, Presidenciavel {
+  String? objetivo;
+
+  Candidato(String? nome, {this.ideologia, this.partido}) : super(nome) {
+    direitosDeveres();
+  }
+
+  @override
+  void objetivosPessoais() {
+    print('$nome tem o objetivo de $objetivo');
+  }
+
+  // INTERFACES
+  @override
+  String? postagem;
+
+  @override
+  void escrevePostagem() {
+    print('Postagem de $nome no facebook: $postagem');
+  }
+
+  @override
+  String? ideologia;
+  String? partido;
+
+  @override
+  void idelologiaPolitica() {
+    print('$nome é candidato com ideologia de $ideologia pelo partido $partido');
+  }
+
+  // Mixins
+  @override
+  void prestacaoContas() {
+    elegivel = super.declaracaoRenda();
+    if (elegivel) {
+      print('Candidato $nome passou na prestação de contas!\nAutorizado a concorrer nas eleições 2022!');
+    } else {
+      print('Candidato $nome foi barrado na prestação de contas!\nSaldo: $saldo excede a renda declarada para Presidencia!');
+    }
+  }
+}
+
+void main() {
+  print('15.0) Mixin Heranças Múltiplas\n');
+
+  var bolsonaro = Candidato('Bolsonaro', ideologia: 'Direita', partido: 'PSL');
+  bolsonaro
+    ..objetivo = 'Ganhar a Eleição'
+    ..objetivosPessoais()
+    ..postagem = 'Vou acabar com a corrupção no Brasil!'
+    ..escrevePostagem()
+    ..idelologiaPolitica()
+    ..depositar = 36000
+    ..prestacaoContas();
+}
