0296dfc0e0618a141775acac0ccd56a7d67fc654
LucasKaosu <lukbrowww@hotmail.com>
2023-07-06 11:36:58 -0300

    Correção no nome do módulo 19

---
+++ 20-Rest/1-Json/Json.dart
@@ -0,0 +1,16 @@
+/**
+ *  * Conceito
+ *    - Representational State Transfer (Transferência Representacional de Estado)
+ *    - É uma arquitetura que define um conjunto de restrições na criação de web services.
+ *    - É uma padronização para multiplas aplicações possam se comunicar usando protocolo HTTP.
+ */
+
+///
+/// * SERIALIZAÇÃO MANUAL - Biblioteca JSON integrada ao dart 'dart:convert';
+///
+/// * Serialização JSON direta
+///   - Perde recursos estatico de linguagens;
+///   - Segurança de tipo, autocompletar e exceções de tempo de compilação.
+///
+
+
